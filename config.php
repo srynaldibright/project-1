b01f14d9541b863163cb3e785f6513046c4d671a
srynaldibright <aldisur123@gmail.com>
2023-06-22 12:35:31 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$koneksi = mysqli_connect('localhost','root','','db_siputaway');
+ //mengecek koneksi
+  //if(!$koneksi){
+ //die("koneksi gagal". mysqli_connect_error());
+ //}
+ //else{
+ //echo "koneksi berhasil"; }
+?>
